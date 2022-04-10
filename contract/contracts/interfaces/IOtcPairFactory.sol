b74a66c731204fd45a9d2ca97b757e67c1700d93
Istvan Rusics <szkkteam1@gmail.com>
2022-04-10 12:53:38 +0200

    initial assumptions

---
+++ contract/contracts/interfaces/IOtcPairFactory.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+interface IOtcPairFactory {
+
+    //function getAllPairsLength() external view returns (uint);
+    //function createPair(address tokenA, address tokenB) external returns (address pair);
+}
